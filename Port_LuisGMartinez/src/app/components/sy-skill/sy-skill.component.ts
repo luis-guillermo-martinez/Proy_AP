ed7c515d3d1e89f88484f599273b2136306d4aa8
Luis G. Martinez <99708664+luis-guillermo-martinez@users.noreply.github.com>
2022-07-05 00:02:50 -0300

    Skills

---
+++ Port_LuisGMartinez/src/app/components/sy-skill/sy-skill.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sy-skill',
+  templateUrl: './sy-skill.component.html',
+  styleUrls: ['./sy-skill.component.css']
+})
+export class SySkillComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
